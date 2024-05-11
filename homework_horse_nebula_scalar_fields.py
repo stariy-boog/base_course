3115ebf0bb6ecf3f1bc62f81677ee0bebe5e1821
Seregachek <111586133+JackEDay@users.noreply.github.com>
2024-05-11 22:31:24 +0000

    2312321321

--- homework_horse_nebula_scalar_fields.py
+++ homework_horse_nebula_scalar_fields.py
@@ -112,7 +112,7 @@ spline_curve = interpolate.splev(figure_spline_part, spline_coords)
 
 plt.axis('equal')
 plt.xlim(0, 350)
-plt.plot(spline_curve[0], spline_curve[1], color='w')
+#plt.plot(spline_curve[0], spline_curve[1], color='w')
 
 curve_coords = []
 points_coords = []
@@ -121,7 +121,7 @@ for i in range(len(spline_curve[0])):
     curve_coords.append([spline_curve[0][i], spline_curve[1][i]])
 
 polygon = geom.Polygon(curve_coords)
-points_number_per_side = 65
+points_number_per_side = 55
 x_pictures_limits = [0, 350]
 y_pictures_limits = [350, 0]
 
@@ -135,18 +135,29 @@ for x_point_coord in np.linspace(*x_pictures_limits, points_number_per_side):
 x_p = np.array(points_coords[0::2])
 y_p = np.array(points_coords[1::2])
 
-def bell_function(x, y, intensity=1, dec_rate=[0.005, 0.005]):
+def bell_function(x, y, intensity=1, dec_rate=[0.5, 0.5]):
     scalar_field = intensity*np.exp(-dec_rate[0] * x**2 - dec_rate[1] * y**2)
     return scalar_field
 
-intensity_centrums_x = [160, 240]
-intensity_centrums_y = [75, 100]
-intensity_values = [100, 60]
+intensity_centrums_x = [160, 170, 180, 190, 200, 215, 230, 250,                       #upper part
+                        190, 190, 200, 195, 205, 195,                                 #middle part 
+                        155,                                                          #lower part
+                        100, 130, 125, 150, 180, 190, 200, 152, 255, 80]              #weak connections
+
+intensity_centrums_y = [72, 75, 78, 81, 84, 85, 87, 95,                               #upper part
+                        130, 130, 200, 180, 210, 225,                                 #middle part 
+                        260,                                                          #lower part
+                        300, 280, 300, 280, 245, 160, 110, 95, 110, 325]              #weak connections
+
+intensity_values = [60, 90, 80, 70, 70, 60, 130, 60,                                  #upper part
+                    70, 30, 120, 60, 60, 50,                                          #middle part 
+                    125,                                                              #lower part
+                    60, 60, 60, 60, 60, 60, 60, 60, 60, 60]                           #weak connections
 
 def scalar_function(x, y, int_cen_x, int_cen_y, int_vel):
     scalar_field = 0
     for i in range(0, len(int_cen_x)):
-        scalar_field += int_vel[i] * bell_function(x - int_cen_x[i], y - int_cen_y[i], 30, [100, 100])
+        scalar_field += int_vel[i] * bell_function(x - int_cen_x[i], y - int_cen_y[i], 0.0372, [0.0035, 0.0035])
     return scalar_field
 
 scalar_fields = []
@@ -160,5 +171,5 @@ ax.set_xlabel('Координата x, м')
 ax.set_ylabel('Координата y, м')
 
 cbar = fig.colorbar(sc_plot)
-cbar.set_label('Интенсивность скалярного поля')
+cbar.set_label('Скорость C^18 O      [КМ/С]')
 plt.savefig("homework_horse_nebula_scalar_fields.png")
